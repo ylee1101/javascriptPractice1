f270a7d3115ba7ce9f9bb830ff83312daf50b3c5
ylee1101 <yongho19911101@hotmail.com>
2018-02-05 18:21:28 -0800

    updated newgame function

--- app.js
+++ app.js
@@ -8,10 +8,124 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var score, roundScore, activePlayer;
+var scores, roundScore, activePlayer, dice, gamePlaying;
 
-score = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init()
 
-dice = 6;
+
+
+
+// create dice
+// dice = Math.floor(Math.random() * 6 + 1);
+// // console.log(dice)
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+
+// // HTML method
+// // document.querySelector("#current-" + activePlayer).innerHTML = "<strong>" + dice + "</strong>";
+// var totalScore = document.querySelector("#score-0").textContent;
+// console.log(totalScore)
+
+
+
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+	if (gamePlaying) {
+		// 1. random number
+		dice = Math.floor(Math.random() * 6) + 1;
+
+		// 2. display the result
+		var diceDOM = document.querySelector(".dice");
+		diceDOM.style.display = "block";
+		diceDOM.src = "dice-" + dice + ".png";
+
+		// 3. update the round score if the rolled number was Not a 1
+		if (dice !== 1) {
+			// add score 
+			roundScore += dice;
+			document.querySelector("#current-" + activePlayer).textContent = roundScore;
+
+		}
+		else {
+			// then next player
+
+			// ternary operator  (? = then)
+			nextPlayer();
+		}
+	}
+
+
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function(){
+	if (gamePlaying) {
+		// add current score to the global score
+		scores[activePlayer] += roundScore;
+
+		// update the UI
+		document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+
+
+		// check if player won the game
+		if (scores[activePlayer] >= 10) {
+			document.querySelector("#name-" + activePlayer).textContent = "WINNER";
+			document.querySelector('.dice').style.display = 'none';
+			// to access winner class we use classList
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+		}
+		else {
+			nextPlayer();
+		}
+	}
+
+})
+
+function nextPlayer () {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById("current-0").textContent = "0";
+	document.getElementById("current-1").textContent = "0";
+
+	document.querySelector(".player-0-panel").classList.toggle("active");
+	document.querySelector(".player-1-panel").classList.toggle("active");
+
+	// document.querySelector(".player-0-panel").classList.remove("active");
+	// document.querySelector(".player-1-panel").classList.add("active");
+
+	document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", init)
+
+function init() {
+	scores = [0, 0];
+	roundScore = 0;
+	activePlayer = 0;
+	gamePlaying = true;
+
+	document.querySelector(".dice").style.display = "none";
+
+	document.getElementById("score-0").textContent = "0"
+	document.getElementById("score-1").textContent = "0"
+	document.getElementById("current-0").textContent = "0"
+	document.getElementById("current-1").textContent = "0"
+
+	document.getElementById("name-0").textContent = "Player 1";
+	document.getElementById("name-1").textContent = "Player 2";
+
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+
+}
+
+// state variable tells condition of statement 
+// ex. is our game playing or not playing?
+
+
